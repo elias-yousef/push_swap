700e9a22e665be3c331d1c59c98801e27ac1173e
elias-yousef <elias2005new@gmail.com>
2026-01-27 21:53:58 +0300

    commit

---
+++ push_swap.c
@@ -0,0 +1,11 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+    while(i < argc)
+    {
+        printf("argc[%d] : %s\n", i, argv[i]);
+        i++;
+    }
+}
---
+++ push_swap.h
@@ -0,0 +1,20 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+typedef struct s_node
+{
+	int	val;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+typedef struct s_stack
+{
+	int		size;
+	t_node	*head;
+}	s_stack;
+
+#endif
